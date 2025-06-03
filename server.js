3aeb05d3f0ab521d09d5d03e17dc5894233600c6
awesomefied <awesomefied.business@gmail.com>
2025-06-02 20:42:39 -0600

    Added auto updates

--- server.js
+++ server.js
@@ -31,6 +31,68 @@ function readFile(path) {
         console.error(err);
     }
 }
+
+async function sha(buffer) {
+    return crypto.subtle.digest("SHA-1", buffer).then((hashBuffer) => {
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray
+            .map((bytes) => bytes.toString(16).padStart(2, "0"))
+            .join("");
+        return hashHex;
+    });
+}
+
+async function checkFile(path, gitHash) {
+    if (fs.existsSync(path)) {
+        const file = fs.readFileSync(path);
+        const fileHeader = new TextEncoder().encode(`blob ${file.length}\0`);
+        const fileBuffer = new Uint8Array(fileHeader.length + file.length);
+        fileBuffer.set(fileHeader);
+        fileBuffer.set(file, fileHeader.length);
+        const fileHash = await sha(fileBuffer);
+        return fileHash == gitHash;
+    } else {
+        return false;
+    }
+}
+
+async function updateFile(path, url) {
+    if (!fs.existsSync(path) && path.split("/").length > 2) {
+        var dir = "./";
+        for (let i = 1; i < path.split("/").length - 1; i++) {
+            dir += path.split("/")[i] + "/";
+            makeFolder(dir);
+        }
+    }
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const buffer = await Buffer.from(await blob.arrayBuffer());
+    fs.writeFileSync(path, buffer);
+}
+
+async function checkUpdates(path, download) {
+    if (!path) {
+        path = "";
+    }
+    const response = await fetch(
+        "https://api.github.com/repos/Awesomefied/AwesomeChat/contents/" + path,
+    );
+    const json = await response.json();
+    for (let i = 0; i < json.length; i++) {
+        if (json[i].type == "file") {
+            const isUpdated = await checkFile("./" + json[i].path, json[i].sha);
+            if (!isUpdated && download) {
+                await updateFile("./" + json[i].path, json[i].download_url);
+            } else if (!isUpdated) {
+                return true;
+            }
+        } else if (json[i].type == "dir") {
+            await checkUpdates(json[i].path, download);
+        }
+    }
+    return false;
+}
+
 makeFolder("./chats");
 
 app.use(express.json({ limit: "50mb" }));
@@ -82,6 +144,16 @@ app.put("/api/htmlviewer", (req, res) => {
     } else {
         res.send(false);
     }
+});
+
+app.get("/api/updatecheck", async (req, res) => {
+    const needUpdate = await checkUpdates();
+    res.send(needUpdate);
+});
+
+app.get("/api/update", async (req, res) => {
+    await checkUpdates("", true);
+    res.send(true);
 });
 
 app.listen(port, () => {
